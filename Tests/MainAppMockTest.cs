413e3a57750593bc4bae52f269669bbcf28f3128
vadevid <78480192+vadevid@users.noreply.github.com>
2022-12-27 17:33:36 +0300

    Add files via upload

---
+++ Tests/MainAppMockTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LabsDB.Entity;
+using MainApp;
+using MainApp.Controller;
+using MainApp.Interfaces;
+using Moq;
+using NUnit.Framework;
+using LabsDB;
+
+namespace Tests.MainApp;
+
+public class MainAppMockTest
+{
+    private readonly IEnumerable<Room> _testRoom;
+
+    public MainAppMockTest()
+    {
+        var testRoom = new List<Room>();
+        var e = new Employee {Id = 1, Login = "123", Password = "123"};
+        for (var i = 1; i < 3; i++)
+        {
+            var room = new Room {Id = i};
+            var ind0 = new Lamp(true, room);
+            room.Lamps.Add(ind0);
+            testRoom.Add(room);
+        }
+
+        _testRoom = testRoom;
+    }
+
+    [Test]
+    public void GetAllRoomSuccess()
+    {
+        var mock = new Mock<IClientService>();
+        mock.Setup(r => r.GetAllRooms()).Returns(_testRoom);
+        var clientController = new ClientController(mock.Object);
+        var result = clientController.GetRoom();
+        Assert.That(result.Count(), Is.EqualTo(_testRoom.Count()));
+    }
+
+    [Test]
+    public void AddNewLampSuccess()
+    {
+        var mock = new Mock<IAgentService>();
+        mock.Setup(r => r.AddNewLamp(It.IsNotNull<Lamp>())).Returns(true);
+        var agentController = new AgentController(mock.Object);
+        var res = agentController.AddNewLamp(new NewRequest());
+        Assert.That(res.Res, Is.False);
+    }
+
+    [Test]
+    public void AddNewLampWithNull()
+    {
+        var mock = new Mock<IAgentService>();
+        mock.Setup(r => r.AddNewLamp(It.IsNotNull<Lamp>())).Returns(true);
+        var agentController = new AgentController(mock.Object);
+        var res = agentController.AddNewLamp(null);
+        Assert.That(res.Res, Is.False);
+    }
+
+    [Test]
+    public void AddNewLampWithErrorRoom()
+    {
+        var mock = new Mock<IAgentService>();
+        mock.Setup(r => r.AddNewLamp(It.Is<Lamp>(i => i.RoomId > 0))).Returns(true);
+        var agentController = new AgentController(mock.Object);
+        var res = agentController.AddNewLamp(new NewRequest{Room = -1});
+        Assert.That(res.Res, Is.False);
+    }
+
+    [Test]
+    public void AddNewLampWithGlowsFalse()
+    {
+        var mock = new Mock<IAgentService>();
+        mock.Setup(r => r.AddNewLamp(It.Is<Lamp>(i => i.Glows))).Returns(true);
+        var agentController = new AgentController(mock.Object);
+        var res = agentController.AddNewLamp(new NewRequest {Glows = false});
+        Assert.That(res.Res, Is.False);
+    }
+
+    [Test]
+    public void AuthSuccess()
+    {
+        var mock = new Mock<IAgentService>();
+        mock.Setup(r => r.AuthEmployee(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(new Employee {Id = 1, Login = "123", Password = "123"});
+        var agentController = new AgentController(mock.Object);
+        var res = agentController.Auth(new AuthRequest{Login = "123", Password = "123"});
+        Assert.That(res, Is.Not.Null);
+    }
+
+    [TestCase("   ", "test")]
+    [TestCase("test", "    ")]
+    [TestCase("   ", "    ")]
+    public void AuthWithErrorData(string login, string password)
+    {
+        var mock = new Mock<IAgentService>();
+        mock.Setup(r => r.AuthEmployee(It.Is<string>(s => !string.IsNullOrWhiteSpace(s)),
+                It.Is<string>(s => !string.IsNullOrWhiteSpace(s))))
+            .Returns(new Employee {Id = 1, Login = "Test", Password = "Test"});
+        var agentController = new AgentController(mock.Object);
+        var res = agentController.Auth(new AuthRequest {Login = login, Password = password});
+        Assert.That(res.Id, Is.EqualTo(-1));
+    }
+    
+    
+    
+    [TestCase(null, "test")]
+    [TestCase("test", null)]
+    [TestCase(null, null)]
+    public void AuthWithNullData(string login, string password)
+    {
+        var mock = new Mock<IAgentService>();
+        mock.Setup(r => r.AuthEmployee(It.Is<string>(s => !string.IsNullOrWhiteSpace(s)),
+                It.Is<string>(s => !string.IsNullOrWhiteSpace(s))))
+            .Returns(new Employee {Id = 1, Login = "Test", Password = "Test"});
+        var agentController = new AgentController(mock.Object);
+        Assert.Catch<ArgumentNullException>(() =>
+            agentController.Auth(new AuthRequest {Login = login, Password = password}));
+    }
+}
---
+++ Tests/MainAppRepositoriesTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LabsDB.Entity;
+using MainApp;
+using MainApp.Services;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace Tests.MainApp;
+
+public class MainAppRepositoriesTest
+{
+    private ApplicationContext _context;
+
+    public MainAppRepositoriesTest()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationContext>()
+            .UseInMemoryDatabase($"ContextDb_{DateTime.Now.ToFileTimeUtc()}").Options;
+        _context = new ApplicationContext(options, true);
+        FillDb();
+    }
+
+    private void FillDb()
+    {
+        var testRoom = new List<Room>();
+        var employee = new Employee {Id = 1, Login = "123", Password = "123"};
+        _context.Employees.Add(employee);
+        for (var i = 1; i < 3; i++)
+        {
+            var room = new Room {Id = i};
+            var ind0 = new Lamp(true, room);
+            room.Lamps.Add(ind0);
+            testRoom.Add(room);
+        }
+
+        _context.AddRange(testRoom);
+        _context.SaveChanges();
+    }
+
+    ~MainAppRepositoriesTest()
+    {
+        _context.Dispose();
+    }
+
+    [Test]
+    public void GetAllRooms()
+    {
+        var service = new ClientService(_context);
+        Assert.That(service.GetAllRooms(), Is.InstanceOf<IEnumerable<Room>>());
+    }
+
+    [Test]
+    public void AddNewLampSuccess()
+    {
+        var service = new AgentService(_context);
+        var room = _context.Room.FirstOrDefault();
+        Assert.That(service.AddNewLamp(new Lamp(true, room)), Is.True);
+    }
+
+    [TestCase("123", "123", ExpectedResult = false)]
+    [TestCase("1233", "123", ExpectedResult = true)]
+    [TestCase("   ", "123", ExpectedResult = true)]
+    [TestCase("123", "    ", ExpectedResult = true)]
+    [TestCase("   ", "    ", ExpectedResult = true)]
+    [TestCase(null, "123", ExpectedResult = true)]
+    [TestCase("123", null, ExpectedResult = true)]
+    [TestCase(null, null, ExpectedResult = true)]
+    public bool AuthWithErrorData(string login, string password)
+    {
+        var service = new AgentService(_context);
+        return service.AuthEmployee(login, password) is null;
+    }
+    
+    [TestCase(1, ExpectedResult = false)]
+    [TestCase(15, ExpectedResult = true)]
+    public bool GetRoomById(int id)
+    {
+        var service = new AgentService(_context);
+        return service.GetRoomById(id) is null;
+    }
+
+    [TestCase(1, ExpectedResult = false)]
+    [TestCase(15, ExpectedResult = true)]
+    public bool GetEmployeeById(int id)
+    {
+        var service = new AgentService(_context);
+        return service.GetEmployeeById(id) is null;
+    }
+}
